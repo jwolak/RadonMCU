796118fee6ae5a3434ba4ad747ff1d05736e928f
Janusz Wolak <jwolak@sipsoftware.pl>
2026-05-26 17:16:43 +0200

    Refactor logging levels in equinios.h for consistency and clarity

--- SoftwareExamplesForRadonMCU/EquiniosLogger/equinios.h
+++ SoftwareExamplesForRadonMCU/EquiniosLogger/equinios.h
@@ -5,19 +5,23 @@
 
 typedef enum
 {
-  LOG_LEVEL_ERROR = 0,
-  LOG_LEVEL_WARN = 1,
-  LOG_LEVEL_INFO = 2,
-  LOG_LEVEL_DEBUG = 3
+  LOG_LEVEL_CRITICAL = 0,
+  LOG_LEVEL_ERROR = 1,
+  LOG_LEVEL_WARNING = 2,
+  LOG_LEVEL_INFO = 3,
+  LOG_LEVEL_DEBUG = 4,
+  LOG_LEVEL_TRACE = 5,
 } log_level_t;
 
 void log_set_level(log_level_t level);
 
 void log_write(log_level_t level, const char *fmt, ...);
 
-#define LOGE(fmt, ...) log_write(LOG_LEVEL_ERROR, "[ERR] " fmt, ##__VA_ARGS__)
-#define LOGW(fmt, ...) log_write(LOG_LEVEL_WARN, "[WRN] " fmt, ##__VA_ARGS__)
-#define LOGI(fmt, ...) log_write(LOG_LEVEL_INFO, "[INF] " fmt, ##__VA_ARGS__)
-#define LOGD(fmt, ...) log_write(LOG_LEVEL_DEBUG, "[DBG] " fmt, ##__VA_ARGS__)
+#define LOGC(fmt, ...) log_write(LOG_LEVEL_CRITICAL, "[CRITICAL] " fmt, ##__VA_ARGS__)
+#define LOGE(fmt, ...) log_write(LOG_LEVEL_ERROR, "[ERROR] " fmt, ##__VA_ARGS__)
+#define LOGW(fmt, ...) log_write(LOG_LEVEL_WARNING, "[WARNING] " fmt, ##__VA_ARGS__)
+#define LOGI(fmt, ...) log_write(LOG_LEVEL_INFO, "[INFO] " fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...) log_write(LOG_LEVEL_DEBUG, "[DEBUG] " fmt, ##__VA_ARGS__)
+#define LOGT(fmt, ...) log_write(LOG_LEVEL_TRACE, "[TRACE] " fmt, ##__VA_ARGS__)
 
 #endif
